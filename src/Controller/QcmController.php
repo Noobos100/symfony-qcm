89de25b01dd2959ea4d8d73da3b649f355c21d69
Noobos100 <charles.daw@etu.univ-amu.fr>
2025-02-04 12:05:45 +0100

    Nom du user dans la page QCM

--- src/Controller/QcmController.php
+++ src/Controller/QcmController.php
@@ -1,18 +1,25 @@
 <?php
 
+// src/Controller/QcmController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class QcmController extends AbstractController
+class QcmController extends AbstractController
 {
     #[Route('/qcm', name: 'app_qcm')]
     public function index(): Response
     {
+        $user = $this->getUser();
+        $email = $user ? $user->getEmail() : 'guest@example.com';
+        $emailFirstPart = explode('@', $email)[0];
+
         return $this->render('qcm/index.html.twig', [
             'controller_name' => 'QcmController',
+            'email_first_part' => $emailFirstPart,
         ]);
     }
 }
