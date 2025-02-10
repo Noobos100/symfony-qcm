190cf322fd814a3206c5d086845dfa79c42168c8
Noobos100 <charles.daw@etu.univ-amu.fr>
2025-02-10 09:25:30 +0100

    add: communication avec l'API (affiche les users)

--- src/Controller/QcmController.php
+++ src/Controller/QcmController.php
@@ -7,9 +7,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class QcmController extends AbstractController
 {
+    private HttpClientInterface $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     #[Route('/', name: 'app_qcm')]
     public function index(): Response
     {
@@ -17,9 +25,15 @@ class QcmController extends AbstractController
         $email = $user ? $user->getEmail() : 'guest@example.com';
         $emailFirstPart = explode('@', $email)[0];
 
+        // Fetch users from the API
+        $response = $this->client->request('GET', $_ENV['API_URL'] . '/users');
+        $data = $response->toArray();
+        $users = $data['member'];
+
         return $this->render('qcm/index.html.twig', [
             'controller_name' => 'QcmController',
             'email_first_part' => $emailFirstPart,
+            'users' => $users,
         ]);
     }
 }
