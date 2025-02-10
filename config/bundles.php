982dbe3452bb2d001199ff41256df4c715f790b4
Roubaud-Axel-2225073b <axel.roubaud@etu.univ-amu.fr>
2025-02-10 20:25:50 +0100

    feat : ajout de tailwinds et debut du css

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
